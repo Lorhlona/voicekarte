5da4673f37760b54b32045f9047605d1f7531441
Lorhlona <mjolnir501@gmail.com>
2024-09-19 21:09:30 +0900

    batファイルを管理者権限で行うとエラーがでるバグを修正いたしました

--- voiceadd/app.bat
+++ voiceadd/app.bat
@@ -1,3 +1,14 @@
-        @echo off
-        cd /d "%~dp0"
-        node note.js
+@echo off
+:: 管理者権限かどうかをチェック
+net session >nul 2>&1
+if %errorLevel% EQU 0 (
+    echo 管理者として実行しないでください。
+    pause
+    exit /b
+)
+
+:: スクリプトのディレクトリに移動
+cd /d "%~dp0"
+
+:: Node.jsアプリケーションの起動
+node note.js
