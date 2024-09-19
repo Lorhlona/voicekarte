4ad94ed7814127983091b22a5ae6cb4736944a62
Lorhlona <mjolnir501@gmail.com>
2024-09-20 00:36:14 +0900

    simple

--- voiceadd/app.bat
+++ voiceadd/app.bat
@@ -1,14 +1,10 @@
 @echo off
 
-echo アプリケーションを起動しています...
 start cmd /k "npm run dev"
 
 
-echo サーバーが起動するまで15秒待機しています...
 timeout /t 15 /nobreak
 
-
-echo ブラウザを開いています...
 start http://localhost:3000
 
 pause
